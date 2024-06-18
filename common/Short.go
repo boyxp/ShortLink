e883278133f62ca77c68a0e8ce3c850ecf945316
lee <isme@live.com>
2024-06-18 13:09:49 +0800

    增加校验位

--- common/Short.go
+++ common/Short.go
@@ -7,24 +7,39 @@ type Short struct {}
 
 func (S Short) Hash(num int64) string {
 	var res []byte
+	var total int64
 	num = num*12345
 	for num>0 {
-		res = append(res, index[num%62])
-		num = num/62
+		mod := num%62
+		res  = append(res, index[mod])
+		num  = num/62
+		total = total+mod
 	}
+
+	res = append(res, index[total%62])
 
 	return string(res)
 }
 
 func (S Short) Convert(hash string) int {
+	if len(hash)<5 {
+		return -1
+	}
+
 	var n int
-	for i:=len(hash)-1;i>=0;i-- {
+	var total int
+	for i:=len(hash)-2;i>=0;i-- {
 		p := strings.Index(index, string(hash[i]))
 		if p==-1 {
-			return 0
+			return -1
 		}
 
 		n = n*62 + p
+		total = total+p
+	}
+
+	if index[total%62]!=hash[len(hash)-1] {
+		return -1
 	}
 
 	return n/12345
